7d1f314e8e16e54f682343ac8a44b0226e9193c5
Trevor_143 <trevorarapu2@gmail.com>
2023-11-01 10:23:31 +0300

    added components

---
+++ src/components/cardList/CardList.jsx
@@ -0,0 +1,7 @@
+import styles from './cardList.module.css'
+
+export default function CardList () {
+    return (
+        <div className={styles.container} >CardList</div>
+    )
+}
---
+++ src/components/menu/Menu.jsx
@@ -0,0 +1,7 @@
+import styles from './navBar.module.css'
+
+export default function NavBar () {
+    return (
+        <div className={styles.container} >NavBar</div>
+    )
+}
