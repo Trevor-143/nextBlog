7e7a1498e5e3b75478f7d9d4337a606d2b2b4443
Trevor_143 <trevorarapu2@gmail.com>
2023-11-13 22:25:16 +0300

    working on mode toggle

--- src/components/themeToggle/ThemeToggle.jsx
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -1,7 +1,15 @@
+"use client"
+
 import Image from "next/image"
 import styles from "./themeToggle.module.css"
+import { useContext } from "react"
+import { ThemeContext } from "@/context/ThemeContext"
 
 export default function ThemeToggle () {
+
+    const { theme } = useContext(ThemeContext)
+
+
     return (
         <div className={styles.container}>
             <Image src="/moon.png" alt="moon" width={14} height={14} />
